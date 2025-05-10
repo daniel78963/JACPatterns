bd845d304e8f84c07890e4aadceff46fae2705f1
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-05-10 12:40:36 -0500

    Add ConsoleSingleton

---
+++ JACPatterns/ConsoleSingleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSingleton
+{
+    public class Singleton
+    {
+        private static readonly Singleton instance = new Singleton("Juan");
+        //private static Singleton instance;
+        public  string texto = String.Empty;
+        public int numerito = -1;
+
+        //public string Texto { get => texto;}
+
+        Singleton(string valor) 
+        {
+            texto = valor;
+        }
+        //Propiedad para acceder a la instancia única
+        public static Singleton Instance
+        {
+            get { return instance; }
+        }
+
+        //public static Singleton GetInstance(string nombre) 
+        //{
+        //    if (instance == null)
+        //        instance = new Singleton(nombre);
+
+        //    return instance;
+        //}
+
+    }
+}
